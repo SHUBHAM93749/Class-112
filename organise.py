ce15b12ad77f77dece279d7fe2a57d396cdbd929
SHUBHAM93749 <88959531+SHUBHAM93749@users.noreply.github.com>
2022-09-04 11:45:04 +0530

    Add files via upload

---
+++ organise.py
@@ -0,0 +1,24 @@
+from distutils import extension
+import os
+import shutil
+from unicodedata import name
+from_dir="C:/Users/HP/Downloads"
+to_dir="D:/org"
+list=os.listdir(from_dir)
+print(list)
+for file in list:
+    name,extension=os.path.splitext(file)
+    if extension == '':
+        continue
+    if extension in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        path1=from_dir+'/'+file
+        path2=to_dir+'/'+'image'
+        path3=to_dir+'/'+'image'+'/'+file
+        if os.path.exists(path2):
+            print("Moving "+file+'...')
+            shutil.move(path1,path3)
+        else:
+            os.makedirs(path2)
+            print("moving "+file+'...')
+            shutil.move(path1,path3)
+
